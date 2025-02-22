470b0e3cb896336e5756d4577177c0adf82057d4
Shubham Pawar <shubhammukundpawar@gmail.com>
2025-02-23 01:04:59 +0530

    Ui added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hii this is Shubham</h1>
+      <div className='flex-container'>
+      <div className='Card-wrapper'>
+        <div className='image-wrapper image ' >
+          <img src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a166pzdhins/gallery/in-galaxy-a16-5g-sm-a166-sm-a166pzdhins-thumb-544007856" alt="" />
+        </div>
+        <div className='text-wrapper'>
+          <span>Samsung A16</span>
+          <span>Price: 100000 Rs.</span>
+        </div>
+        <div>
+          <button className='button'> Add to Cart</button>
+        </div>
+
+      </div>
+
+
+      <div className='Card-wrapper'>
+        <div className='image-wrapper image '>
+          <img src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a166pzdhins/gallery/in-galaxy-a16-5g-sm-a166-sm-a166pzdhins-thumb-544007856" alt="" />
+        </div>
+        <div className='text-wrapper'>
+          <span>Samsung A16</span>
+          <span>Price: 100000 Rs.</span>
+        </div>
+        <div>
+          <button className='button'> Add to Cart</button>
+        </div>
+
+      </div>
+
+      <div className='Card-wrapper'>
+        <div className='image-wrapper  image'>
+          <img src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a166pzdhins/gallery/in-galaxy-a16-5g-sm-a166-sm-a166pzdhins-thumb-544007856" alt="" />
+        </div>
+        <div className='text-wrapper'>
+          <span>Samsung A16</span>
+          <span>Price: 100000 Rs.</span>
+        </div>
+        <div>
+          <button className='button'> Add to Cart</button>
+        </div>
+
+      </div>
+
+      <div className='Card-wrapper'>
+        <div className='image-wrapper image'>
+          <img src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a166pzdhins/gallery/in-galaxy-a16-5g-sm-a166-sm-a166pzdhins-thumb-544007856" alt="" />
+        </div>
+        <div className='text-wrapper'>
+          <span>Samsung A16</span>
+          <span>Price: 100000 Rs.</span>
+        </div>
+        <div>
+          <button className='button'> Add to Cart</button>
+        </div>
+
+      </div>
+
+      <div className='Card-wrapper'>
+        <div className='image-wrapper image2'>
+          <img src="https://images.samsung.com/is/image/samsung/p6pim/in/sm-a166pzdhins/gallery/in-galaxy-a16-5g-sm-a166-sm-a166pzdhins-thumb-544007856" alt="" />
+        </div>
+        <div className='text-wrapper'>
+          <span>Samsung A16</span>
+          <span>Price: 100000 Rs.</span>
+        </div>
+        <div>
+          <button className='button'> Add to Cart</button>
+        </div>
+
+      </div>
+      </div>
+    </div>
+  )
+}
